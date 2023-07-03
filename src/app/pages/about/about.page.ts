efdef2fff63408e177af1f0e8708616f0e9e476a
singharach <singharach.sa.63@ubu.ac.th>
2023-07-03 12:57:13 +0700

    add new

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular/providers/alert-controller';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  productlist: any;
+  constructor(private alertCtrl: AlertController) {
+    this.productlist = [
+      {
+        id: 1,
+        productname: "Perfume",
+        price: "1055",
+        icon: "https://img.icons8.com/color/48/perfume-bottle.png"
+      },
+      {
+        id: 2,
+        productname: "Shampoo",
+        price: "355",
+        icon: "https://img.icons8.com/color/48/shampoo.png"
+      },
+      {
+        id: 3,
+        productname: "Nail Polish",
+        price: "125",
+        icon: "https://img.icons8.com/color/48/nail-polish.png"
+      }
+    ];
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
 
+  async showedit(item: any) {
+    let alert = this.alertCtrl.create({
+      header: 'Edit',
+      subHeader: "Fill the form",
+      inputs: [
+        {
+          name: 'inpname',
+          placeholder: 'product name',
+          value: item.productname
+        },
+        {
+          name: 'password',
+          placeholder: 'Password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+
+          }
+        }
+      ]
+    });
+    (await alert).present();
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -13,23 +13,44 @@ export class HomePage implements OnInit {
   head: any;
   content: any;
   dt: any;
+  currentDate: any;
   constructor(public navCtrl: NavController,
-    public router: Router) { }
+    public router: Router) {
+      this.currentDate = {
+        date: "this.currentD"
+      }
+     }
 
   ngOnInit() {
   }
 
-  gotonext(){
+  gotonext() {
     // this.router.navigate(['homeresult', 
     //    this.name , this.head , this.content , this.dt]);
-    let dataobj={
-      getname:this.name,
-      gethead:this.head,
-      getcontent:this.content,
-      getdt:this.dt
+    let dataobj = {
+      getname: this.name,
+      gethead: this.head,
+      getcontent: this.content,
+      getdt: this.dt
     };
     let datastr = JSON.stringify(dataobj);
-    this.router.navigate(['homeresult',datastr])
+    this.router.navigate(['homeresult', datastr])
+  }
+
+  getday() {
+    const date = new Date();
+
+    let currentDay = String(date.getDate()).padStart(2, '0');
+
+    let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+
+    let currentYear = date.getFullYear();
+
+    // we will display the date as DD-MM-YYYY 
+
+    let currentD = `${currentDay}-${currentMonth}-${currentYear}`;
+
+    console.log(currentD)
   }
 
 }
